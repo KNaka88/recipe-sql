d356671e6d065304f34941f9306897c680571073
Jennifer Beem and Koji Nakagawa <koji@>
2017-02-20 15:03:37 -0800

    add html and display paths

--- app/app.php
+++ app/app.php
@@ -1,31 +1,36 @@
 <?php
+    date_default_timezone_set("America/Los_angeles");
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Recipe.php";
+    require_once __DIR__."/../src/Ingredients.php";
+    require_once __DIR__."/../src/Cuisine.php";
+
 
     $app = new Silex\Application();
 
-    $server = 'mysql:host=localhost:8889;dbname=to_do';
+    $server = 'mysql:host=localhost:8889;dbname=recipe_database';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
+    $app['debug'] = true;
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+        return $app['twig']->render('recipes.html.twig', array('recipes' => Recipe::getAll()));
     });
 
-    $app->post("/tasks", function() use ($app) {
-        $task = new Task($_POST['description']);
-        $task->save();
-        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+    $app->post("/recipes", function() use ($app) {
+        $recipe = new Recipe($_POST['name'], $_POST['rating']);
+        $recipe->save();
+        return $app['twig']->render('create_recipe.html.twig', array('newrecipes' => $recipe));
     });
 
-    $app->post("/delete_tasks", function() use ($app) {
-        Task::deleteAll();
-        return $app['twig']->render('delete_tasks.html.twig');
+    $app->post("/delete_recipe", function() use ($app) {
+        Recipe::deleteAll();
+        return $app['twig']->render('delete_recipes.html.twig');
     });
 
 
